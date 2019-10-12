9c12c0492ffd22c39ea36287c6a6a0b9601514fb
namickey <teethdai@gmail.com>
2019-10-12 13:22:17 +0900

    fix

--- 1_basic/netcat.py
+++ 1_basic/netcat.py
@@ -4,6 +4,7 @@ import socket
 import getopt
 import threading
 import subprocess
+import time
 
 listen = False
 command = False
@@ -53,6 +54,7 @@ def main():
             assert False, "Unhandled Option"
     if not listen and len(target) and port > 0:
         buffer = sys.stdin.read()
+        print(buffer)
         client_sender(buffer)
     if listen:
         server_loop()
@@ -62,23 +64,32 @@ def client_sender(buffer):
     try:
         client.connect((target, port))
         if len(buffer):
-            client.send(buffer)
+            print("send1")
+            client.send(buffer.encode())
+        time.sleep(1)
         while True:
             recv_len = 1
             response = ""
             while recv_len:
+                print("recv1")
                 data = client.recv(4096)
                 recv_len = len(data)
-                response += data
+                response += data.decode()
                 if recv_len < 4096:
                     break
-            print(response)
-            buffer = raw_input("")
+            #print(response)
+            buffer = input()
+            print("raw_input:" + buffer)
             buffer += "\n"
-            client.send(buffer)
-    except:
+            print("send2")
+            client.send(buffer.encode())
+            if "exit\n" == buffer:
+                break
+    except Exception as e:
+        print(e)
         print("[*] Exception! Existing.")
-        client.close()
+        client.send("exit\n".encode())
+    client.close()
 
 def server_loop():
     global target
@@ -87,57 +98,67 @@ def server_loop():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((target, port))
     server.listen(5)
-    print('11')
     while True:
         client_socket, addr = server.accept()
         print("[*] Accepted connection from: %s:%d" % (addr[0], addr[1]))
-        print('12')
         client_thread = threading.Thread(target=client_handler, args=(client_socket,))
         client_thread.start()
-        print('13')
 
 def run_command(command):
     command = command.rstrip()
     try:
+        print("command:" + command)
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
-    except:
+        print("output:" + output.decode())
+    except Exception as e:
+        print(e)
         output = "Failed to execute command.\r\n"
     return output
 
 def client_handler(client_socket):
     global upload
+    global upload_destination
     global execute
     global command
-    print("21")
     if len(upload_destination):
-        file_buffer = ""
+        file_buffer = "".encode()
         while True:
+            print("recv9")
             data = client_socket.recv(1024)
-            if len(data) == 0:
+            print("data len:" + str(len(data)))
+            file_buffer += data
+            if len(data) < 1024:
                 break
-            else:
-                file_buffer += data
         try:
             file_descriptor = open(upload_destination, "wb")
             file_descriptor.write(file_buffer)
             file_descriptor.close()
-            client_socket.send("Successfully saved file to %s\r\n % upload_destination")
+            print("send9")
+            client_socket.send(("Successfully saved file to %s\r\n" % upload_destination).encode())
         except:
-            client_socket.send("Failed to save file to %s\r\n % upload_destination")
+            print("send8")
+            client_socket.send(("Failed to save file to %s\r\n" % upload_destination).encode())
+        upload_destination = ""
     if len(execute):
         output = run_command(execute)
         client_socket.send(output)
     if command:
         prompt = "<BHP:#> "
+        print("send7")
         client_socket.send(prompt.encode())
         while True:
             cmd_buffer = ""
             while "\n" not in cmd_buffer:
-                s = str(client_socket.recv(1024))
-                print(s)
-                cmd_buffer += s
-            response = run_command(cmd_buffer)
+                time.sleep(1)
+                print("recv8")
+                cmd_buffer += client_socket.recv(1024).decode()
+                print("cmd_buffer:" + cmd_buffer)
+            if "exit\n" == cmd_buffer:
+                break
+            response = run_command(cmd_buffer).decode()
             response += prompt
-            client_socket.send(response)
+            print("send6")
+            client_socket.send(response.encode())
+    #client_socket.close()
 
 main()
